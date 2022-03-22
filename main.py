33245705f8c9372394437d7b2769b420a4bf07d8
saladbowl77 <sarakun.info@gmail.com>
2022-03-22 21:58:28 +0900

    fix : datetime

--- main.py
+++ main.py
@@ -38,58 +38,59 @@ cityWarningData = [{
 """
 
 dirName = dirname(abspath(__file__))
-#時刻周りの生成
-#now : 現在時刻
-#jmaUpdate : 気象庁XMLアップデート
-#jmaLastUpdate : 前回のアップデート
-now = datetime.now(timezone(timedelta(hours=9)))
-jmaUpdate = datetime.strptime(cityWarningData[1]['update'], '%Y-%m-%dT%H:%M:%S%z')
-with open(f'{dirName}/tmp.txt') as f:
-    jmaLastUpdate = f.read()
-    jmaLastUpdate = datetime.strptime(jmaLastUpdate, '%Y-%m-%dT%H:%M:%S%z')
+if 'update' in cityWarningData[1]:
+    #時刻周りの生成
+    #now : 現在時刻
+    #jmaUpdate : 気象庁XMLアップデート
+    #jmaLastUpdate : 前回のアップデート
+    now = datetime.now(timezone(timedelta(hours=9)))
+    jmaUpdate = datetime.strptime(cityWarningData[1]['update'], '%Y-%m-%dT%H:%M:%S%z')
+    with open(f'{dirName}/tmp.txt') as f:
+        jmaLastUpdate = f.read()
+        jmaLastUpdate = datetime.strptime(jmaLastUpdate, '%Y-%m-%dT%H:%M:%S%z')
 
-#警報等が一つ以上ある場合で前回より更新しているまたは、0分の場合
-if cityWarningData[1]['cityCount'] > 0 and (jmaUpdate > jmaLastUpdate or now.minute == 0):
-    wLevels = mkMap(cityWarningData[0])
-    imageList = mkImage(cityWarningData[0])
-    #画像リスト
-    ids = []
-    for path in imageList:
-        id = uploadImage(path)
-        ids.append(id)
-    #以下ツイート文生成→ツイート
-    tweetText = f'{now.day}日{now.hour}時{now.minute}分現在 #滋賀県 内'
-    if cityWarningData[1]['cityCount'] == 20:
-        tweetText += '全域に'
-    else:
-        tweetText += '一部地域に'
-    alarts = []
-    if wLevels[0]:
-        alarts.append(' #特別警報 ')
-    if wLevels[1]:
-        alarts.append(' #警報 ')
-    if wLevels[2]:
-        alarts.append(' #注意報 ')
-    tweetText += '、'.join(alarts) + 'が発令されています。\n'
-    if wLevels[0]:
-        tweetText += '大切な命を守るため、身の安全を確保してください。\n'
-    tweetText += '#滋賀県気象情報\n'
-    tweetText += f'(気象庁更新時刻 {jmaUpdate.day}日{jmaUpdate.hour}時{jmaUpdate.minute}分)'
-    if args == ['main.py']:
-        tweet(tweetText,mediaIDs=ids)
-    elif args[1] == 'gitTest':
-        print(tweetText)
-    #画像の削除
-    for path in imageList:
-        remove(path)
-    remove(f'{dirName}/warningMap.png')
-#警報等がなくなった場合で前回のアップデート時間と違う場合
-elif cityWarningData[1]["endWarning"] and  jmaUpdate > jmaLastUpdate:
-    tweetText = f'{now.day}日{now.hour}時{now.minute}分現在 #滋賀県 内に発令されていた #注意報 #警報 は解除されました。 #滋賀県気象情報\n(気象庁更新時刻 {jmaUpdate.day}日{jmaUpdate.hour}時{jmaUpdate.minute}分)'
-    if args == ['main.py']:
-        tweet(tweetText)
-    elif args[1] == 'gitTest':
-        print(tweetText)
-#気象庁のアップデート時間の更新(更新の有無に関わらず)
-with open(f'{dirName}/tmp.txt', mode='w') as f:
-    f.write(cityWarningData[1]['update'])
+    #警報等が一つ以上ある場合で前回より更新しているまたは、0分の場合
+    if cityWarningData[1]['cityCount'] > 0 and (jmaUpdate > jmaLastUpdate or now.minute == 0):
+        wLevels = mkMap(cityWarningData[0])
+        imageList = mkImage(cityWarningData[0])
+        #画像リスト
+        ids = []
+        for path in imageList:
+            id = uploadImage(path)
+            ids.append(id)
+        #以下ツイート文生成→ツイート
+        tweetText = f'{now.day}日{now.hour}時{now.minute}分現在 #滋賀県 内'
+        if cityWarningData[1]['cityCount'] == 20:
+            tweetText += '全域に'
+        else:
+            tweetText += '一部地域に'
+        alarts = []
+        if wLevels[0]:
+            alarts.append(' #特別警報 ')
+        if wLevels[1]:
+            alarts.append(' #警報 ')
+        if wLevels[2]:
+            alarts.append(' #注意報 ')
+        tweetText += '、'.join(alarts) + 'が発令されています。\n'
+        if wLevels[0]:
+            tweetText += '大切な命を守るため、身の安全を確保してください。\n'
+        tweetText += '#滋賀県気象情報\n'
+        tweetText += f'(気象庁更新時刻 {jmaUpdate.day}日{jmaUpdate.hour}時{jmaUpdate.minute}分)'
+        if args == ['main.py']:
+            tweet(tweetText,mediaIDs=ids)
+        elif args[1] == 'gitTest':
+            print(tweetText)
+        #画像の削除
+        for path in imageList:
+            remove(path)
+        remove(f'{dirName}/warningMap.png')
+    #警報等がなくなった場合で前回のアップデート時間と違う場合
+    elif cityWarningData[1]["endWarning"] and  jmaUpdate > jmaLastUpdate:
+        tweetText = f'{now.day}日{now.hour}時{now.minute}分現在 #滋賀県 内に発令されていた #注意報 #警報 は解除されました。 #滋賀県気象情報\n(気象庁更新時刻 {jmaUpdate.day}日{jmaUpdate.hour}時{jmaUpdate.minute}分)'
+        if args == ['main.py']:
+            tweet(tweetText)
+        elif args[1] == 'gitTest':
+            print(tweetText)
+    #気象庁のアップデート時間の更新(更新の有無に関わらず)
+    with open(f'{dirName}/tmp.txt', mode='w') as f:
+        f.write(cityWarningData[1]['update'])
