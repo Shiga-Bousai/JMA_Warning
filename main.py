60b7d84cde5c6b25ca57145b6f347acc9a48d9c1
saladbowl77 <sarakun.info@gmail.com>
2022-03-27 16:23:38 +0900

    add & fix : landslideAlertInfo

--- main.py
+++ main.py
@@ -1,96 +1,28 @@
-from warning import getWarning, mkMap, mkImage
+from warning import getWarning
 from os import remove
 from datetime import datetime, timezone, timedelta
-
 from os.path import dirname, abspath
 import sys
-sys.path.append(abspath("../"))
-from pkg.twitter_python import tweet,uploadImage
+
+now = datetime.now(timezone(timedelta(hours=9)))
 dirName = dirname(abspath(__file__))
-
 args = sys.argv
-print(args)
 
-cityWarningData = getWarning()
-"""
-#デバッグ用でツイートしないよう注意
-cityWarningData = [{
-    '2520101': ['02','22','27','32','12','13','14','15'],
-    '2520102': ['22'],
-    '2520200': ['22'],
-    '2520300': ['22'],
-    '2520400': ['10'],
-    '2520600': ['15'],
-    '2520700': ['18'],
-    '2520800': ['18'],
-    '2520900': ['18'],
-    '2521000': ['22'],
-    '2521100': ['21'],
-    '2521200': ['22'],
-    '2521300': ['32'],
-    '2521400': ['22'],
-    '2538300': ['12'],
-    '2538400': ['14'],
-    '2542500': ['22'],
-    '2544100': ['14'],
-    '2544200': ['12'],
-    '2544300': ['22']
-},{'update': '2022-03-13T15:57:00+09:00', 'cityCount': 20, 'endWarning' : False}]
-"""
+#時刻周りの生成
+#now : 現在時刻
+#jmaLastUpdate : 前回のアップデート
+with open(f'{dirName}/tmp.txt') as f:
+    jmaLastUpdate = f.read()
+    if jmaLastUpdate != "":
+        jmaLastUpdateDatetime = datetime.strptime(jmaLastUpdate, '%Y-%m-%dT%H:%M:%S%z')
+    else:
+        jmaLastUpdateDatetime = now + timedelta(minutes=-5)
 
-if 'update' in cityWarningData[1]:
-    #時刻周りの生成
-    #now : 現在時刻
-    #jmaUpdate : 気象庁XMLアップデート
-    #jmaLastUpdate : 前回のアップデート
-    now = datetime.now(timezone(timedelta(hours=9)))
-    jmaUpdate = datetime.strptime(cityWarningData[1]['update'], '%Y-%m-%dT%H:%M:%S%z')
-    with open(f'{dirName}/tmp.txt') as f:
-        jmaLastUpdate = f.read()
-        jmaLastUpdate = datetime.strptime(jmaLastUpdate, '%Y-%m-%dT%H:%M:%S%z')
+updatedDatetime = getWarning(jmaLastUpdateDatetime, args)
 
-    #警報等が一つ以上ある場合で前回より更新しているまたは、0分の場合
-    if cityWarningData[1]['cityCount'] > 0 and (jmaUpdate > jmaLastUpdate or now.minute == 0):
-        wLevels = mkMap(cityWarningData[0])
-        imageList = mkImage(cityWarningData[0])
-        #画像リスト
-        ids = []
-        for path in imageList:
-            id = uploadImage(path)
-            ids.append(id)
-        #以下ツイート文生成→ツイート
-        tweetText = f'{now.day}日{now.hour}時{now.minute}分現在 #滋賀県 内'
-        if cityWarningData[1]['cityCount'] == 20:
-            tweetText += '全域に'
-        else:
-            tweetText += '一部地域に'
-        alarts = []
-        if wLevels[0]:
-            alarts.append(' #特別警報 ')
-        if wLevels[1]:
-            alarts.append(' #警報 ')
-        if wLevels[2]:
-            alarts.append(' #注意報 ')
-        tweetText += '、'.join(alarts) + 'が発令されています。\n'
-        if wLevels[0]:
-            tweetText += '大切な命を守るため、身の安全を確保してください。\n'
-        tweetText += '#滋賀県気象情報\n'
-        tweetText += f'(気象庁更新時刻 {jmaUpdate.day}日{jmaUpdate.hour}時{jmaUpdate.minute}分)'
-        if args == ['main.py']:
-            tweet(tweetText,mediaIDs=ids)
-        elif args[1] == 'gitTest':
-            print(tweetText)
-        #画像の削除
-        for path in imageList:
-            remove(path)
-        remove(f'{dirName}/warningMap.png')
-    #警報等がなくなった場合で前回のアップデート時間と違う場合
-    elif cityWarningData[1]["endWarning"] and  jmaUpdate > jmaLastUpdate:
-        tweetText = f'{now.day}日{now.hour}時{now.minute}分現在 #滋賀県 内に発令されていた #注意報 #警報 は解除されました。 #滋賀県気象情報\n(気象庁更新時刻 {jmaUpdate.day}日{jmaUpdate.hour}時{jmaUpdate.minute}分)'
-        if args == ['main.py']:
-            tweet(tweetText)
-        elif args[1] == 'gitTest':
-            print(tweetText)
-    #気象庁のアップデート時間の更新(更新の有無に関わらず)
-    with open(f'{dirName}/tmp.txt', mode='w') as f:
-        f.write(cityWarningData[1]['update'])
+#気象庁のアップデート時間の更新(更新の有無に関わらず)
+with open(f'{dirName}/tmp.txt', mode='w') as f:
+    if updatedDatetime == None:
+        f.write(jmaLastUpdate)
+    else:
+        f.write(updatedDatetime)
--- warning.py
+++ warning.py
@@ -1,44 +1,48 @@
+from datetime import datetime, timezone, timedelta
+
 import requests
+import xmltodict
+from PIL import Image, ImageDraw, ImageFont #pip install pillow-simd
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
+from os import remove
 from os.path import dirname, abspath, join
+import sys
+sys.path.append(abspath("../"))
+from pkg.twitter_python import tweet,uploadImage
 dirName = dirname(abspath(__file__))
 
 imageBaseColor = "#313131"
 
-import xmltodict
+fontpathGSG_B = join(dirName,abspath("../fonts/GenShinGothic-Bold.ttf"))
+fontGSG_B35 = ImageFont.truetype(fontpathGSG_B, 35)
+fontGSG_B40 = ImageFont.truetype(fontpathGSG_B, 40)
+fontGSG_B50 = ImageFont.truetype(fontpathGSG_B, 50)
+
 def jmaAPI(url):
     jmaReq = requests.get(url)
     jmaReq.encoding = 'utf-8'
     return xmltodict.parse(jmaReq.text)
 
-def getWarning():
-    cityWarningCode = {"2520101":[],"2520102":[],"2520200":[],"2520300":[],"2520400":[],"2520600":[],"2520700":[],"2520800":[],"2520900":[],"2521000":[],"2521100":[],"2521200":[],"2521300":[],"2521400":[],"2538300":[],"2538400":[],"2542500":[],"2544100":[],"2544200":[],"2544300":[]}
-    otherData = {}
-    wCount = 0
-    jmaList = jmaAPI('https://www.data.jma.go.jp/developer/xml/https_feed/extra.xml')
+def getWarning(lastUpdateTime, args):
+    newestUpdateDatetime = None
+    jmaList = jmaAPI('https://www.data.jma.go.jp/developer/xml/feed/extra_l.xml')
     for listData in jmaList["feed"]["entry"]:
-        if listData["author"]["name"] == '彦根地方気象台' and listData["title"] == '気象特別警報・警報・注意報':
-            otherData["update"] = listData["updated"] #アップロード時間追加
-            otherData["endWarning"] = True #解除 注意報なしチェック
-            jmaDetail = jmaAPI(listData["id"])
-            for data in jmaDetail["Report"]["Body"]["Warning"][3]["Item"]:
-                cityCode = data["Area"]["Code"] #市町識別コード
-                if type(data["Kind"]) == list:
-                    for warning in data["Kind"]:
-                        if warning["Status"] not in ["解除", "発表警報・注意報はなし"]:
-                            otherData["endWarning"] = False
-                            wCount+=1
-                            cityWarningCode[cityCode].append(warning["Code"])
-                else:
-                    if data["Kind"]["Status"] not in ["解除", "発表警報・注意報はなし"]:
-                        otherData["endWarning"] = False
-                        wCount+=1
-                        cityWarningCode[cityCode].append(data["Kind"]["Code"])
-            break
-    otherData['cityCount'] = wCount
-    return [cityWarningCode, otherData]
+        updateDatetime = datetime.strptime(listData["updated"], '%Y-%m-%dT%H:%M:%S%z')
+        if listData["author"]["name"] == '彦根地方気象台' and updateDatetime >= lastUpdateTime:
+            updateBool =  updateDatetime > lastUpdateTime
+            if newestUpdateDatetime == None:
+                newestUpdateDatetime = listData["updated"]
+            if listData["title"] == '気象特別警報・警報・注意報':
+                jmaDetail = jmaAPI(listData["id"])
+                weatherWarningData(jmaDetail, updateBool, args)
+            elif listData["title"] == '土砂災害警戒情報':
+                jmaDetail = jmaAPI(listData["id"])
+                landslideAlertInfo(jmaDetail, updateBool, args)
+                
+    return newestUpdateDatetime
 
-import matplotlib.pyplot as plt
-import geopandas as gpd
 def mkMap(cityCodeDict):
     shigaCity = gpd.read_file(f'{dirName}/mapData/N03-20210101_25_GML.geojson') #滋賀県内の市町データ取得
     ax = plt.subplots(1, 1, figsize=(8, 8))[1]
@@ -141,19 +145,14 @@ def mkImage(cityWarningData):
         "38" : "高潮"
     }
 
-    from PIL import Image, ImageDraw, ImageFont #pip install pillow-simd
-
     BaseBG = Image.new('RGBA', (1920, 1080), (49, 49, 49, 255))
     draw = ImageDraw.Draw(BaseBG)
-    fontpathGSG_B = join(dirName,abspath("../fonts/GenShinGothic-Bold.ttf"))
 
     warningMap = Image.open(f'{dirName}/warningMap.png')
     warningMap = warningMap.crop((500, 0, 1900, 2400))
     warningMap = warningMap.resize((630, 1080))
 
     BaseBG.paste(warningMap)
-    fontGSG_B40 = ImageFont.truetype(fontpathGSG_B, 40)
-    fontGSG_B50 = ImageFont.truetype(fontpathGSG_B, 50)
 
     warnCityCount = 0
     textH = 100
@@ -196,3 +195,157 @@ def mkImage(cityWarningData):
     else:
         BaseBG.save(f'{dirName}/WarningPost2.png')
         return [f'{dirName}/WarningPost1.png',f'{dirName}/WarningPost2.png']
+
+def weatherWarningData(jmaDetail, updateBool, args):
+    now = datetime.now(timezone(timedelta(hours=9)))
+    otherData = {}
+    wCount = 0
+    otherData["endWarning"] = True #解除 注意報なしチェック
+    updatedDatetime = jmaDetail["Report"]["Control"]["DateTime"]
+    otherData["update"] = updatedDatetime #アップロード時間追加
+    cityWarningCode = {"2520101":[],"2520102":[],"2520200":[],"2520300":[],"2520400":[],"2520600":[],"2520700":[],"2520800":[],"2520900":[],"2521000":[],"2521100":[],"2521200":[],"2521300":[],"2521400":[],"2538300":[],"2538400":[],"2542500":[],"2544100":[],"2544200":[],"2544300":[]}
+    for data in jmaDetail["Report"]["Body"]["Warning"][3]["Item"]:
+        cityCode = data["Area"]["Code"] #市町識別コード
+        if type(data["Kind"]) == list:
+            for warning in data["Kind"]:
+                if warning["Status"] not in ["解除", "発表警報・注意報はなし"]:
+                    otherData["endWarning"] = False
+                    wCount+=1
+                    cityWarningCode[cityCode].append(warning["Code"])
+        else:
+            if data["Kind"]["Status"] not in ["解除", "発表警報・注意報はなし"]:
+                otherData["endWarning"] = False
+                wCount+=1
+                cityWarningCode[cityCode].append(data["Kind"]["Code"])
+    otherData['cityCount'] = wCount
+
+    #jmaUpdate : 気象庁XMLアップデート時間
+    jmaUpdate = datetime.strptime(otherData['update'], '%Y-%m-%dT%H:%M:%S%z')
+    #警報等が一つ以上ある場合で前回より更新しているまたは、0分の場合
+    if otherData['cityCount'] > 0 and (updateBool or now.minute == 0):
+        wLevels = mkMap(cityWarningCode)
+        imageList = mkImage(cityWarningCode)
+        #画像リスト
+        ids = []
+        for path in imageList:
+            id = uploadImage(path)
+            ids.append(id)
+        #以下ツイート文生成→ツイート
+        tweetText = f'{now.day}日{now.hour}時{now.minute}分現在 #滋賀県 内'
+        if otherData['cityCount'] == 20:
+            tweetText += '全域に'
+        else:
+            tweetText += '一部地域に'
+        alarts = []
+        if wLevels[0]:
+            alarts.append(' #特別警報 ')
+        if wLevels[1]:
+            alarts.append(' #警報 ')
+        if wLevels[2]:
+            alarts.append(' #注意報 ')
+        tweetText += '、'.join(alarts) + 'が発令されています。\n'
+        if wLevels[0]:
+            tweetText += '大切な命を守るため、身の安全を確保してください。\n'
+        tweetText += '#滋賀県気象情報\n'
+        tweetText += f'(気象庁更新時刻 {jmaUpdate.day}日{jmaUpdate.hour}時{jmaUpdate.minute}分)'
+        if args == ['main.py']:
+            tweet(tweetText,mediaIDs=ids)
+        elif args[1] == 'gitTest':
+            print(tweetText)
+        #画像の削除
+        for path in imageList:
+            remove(path)
+        remove(f'{dirName}/warningMap.png')
+    #警報等がなくなった場合で前回のアップデート時間と違う場合
+    elif updateBool and otherData["endWarning"]:
+        tweetText = f'{now.day}日{now.hour}時{now.minute}分現在 #滋賀県 内に発令されていた #注意報 #警報 は解除されました。 #滋賀県気象情報\n(気象庁更新時刻 {jmaUpdate.day}日{jmaUpdate.hour}時{jmaUpdate.minute}分)'
+        if args == ['main.py']:
+            tweet(tweetText)
+        elif args[1] == 'gitTest':
+            print(tweetText)
+
+def landslideAlertInfo(jmaLLADetail, updateBool, args):
+    now = datetime.now(timezone(timedelta(hours=9)))
+    endedBool = False
+    cityCount = False
+    #データのJSON化
+    alertLv = {
+        "発表" : [],
+        "継続" : [],
+        "解除" : []
+    }
+    for name in jmaLLADetail["Report"]["Body"]["Warning"]["Item"]:
+        if name["Kind"]["Code"] in ['1','3']:
+            if endedBool == False and name["Kind"]["Code"] == '3':
+                endedBool = True
+            cityCount = True
+            alertLv[name["Kind"]["Status"]].append(name["Area"]["Name"])
+    
+    #一市町以上ある場合で、更新されたor0分の場合
+    if cityCount and (updateBool or now.minute == 0):
+        #Baseになる画像読み込み
+        BaseBG = Image.open(f'{dirName}/base_img/landslideAlertInfo.jpeg')
+        draw = ImageDraw.Draw(BaseBG)
+        baseTop = 200
+        baseLeft = 100
+
+        #気象庁電文描画
+        headLineText = jmaLLADetail["Report"]["Head"]["Headline"]["Text"]
+        #改行ごとにarray化
+        headLineText = headLineText.split('\n')
+        #一文字ごとに描画する
+        for headLineTextArr in headLineText:
+            for hl_text in headLineTextArr:
+                textBox = draw.multiline_textbbox((0,0), hl_text, font=fontGSG_B35)
+                textW = textBox[2] - textBox[0]
+                if baseLeft + textW > 1850:
+                    baseTop += 50
+                    baseLeft = 100
+                draw.multiline_text((baseLeft,baseTop), hl_text, font=fontGSG_B35,fill='#efefef',anchor='lm')
+                baseLeft += textW
+            baseTop += 50
+            baseLeft = 100
+        baseTop += 10
+
+        #電文と内容の分割線
+        draw.rectangle((baseLeft, baseTop,1880,baseTop + 2), fill='#efefef')
+
+        #内容の描画
+        for lvName in alertLv:
+            if alertLv[lvName] != []:
+                baseTop += 60
+                draw.multiline_text((baseLeft,baseTop), f'{lvName} : ', font=fontGSG_B40,fill='#efefef',anchor='lm')
+                baseLeft = 220
+                for alertData in alertLv[lvName]:
+                    textBox = draw.multiline_textbbox((0,0), alertData, font=fontGSG_B40)
+                    textW = textBox[2] - textBox[0]
+                    if baseLeft + textW > 1850:
+                        baseTop += 60
+                        baseLeft = 220
+                    draw.multiline_text((baseLeft,baseTop), alertData, font=fontGSG_B40,fill='#efefef',anchor='lm')
+                    baseLeft += textW + 10
+                baseTop += 20
+                baseLeft = 100
+
+        #ファイル保存からのアップロード
+        wImagePath = f'{dirName}/landslideAlertInfo.jpeg'
+        BaseBG.save(wImagePath)
+        if alertLv["発表"] != [] or alertLv["継続"] != []:
+            tweetText = """#土砂災害警戒情報 発令中
+土砂災害警戒区域等では土砂災害が発生しやすい状況になっています。
+ハザードマップなどを確認し災害に備えてください。
+
+滋賀県のハザードマップはこちら↓
+https://shiga-bousai.jp/dmap/map/index?l=M_r_k_d_risk_map&f=0010011111101000000&b=google_base&z=10&x=15156480.5&y=4197618.7349338
+"""     
+        elif alertLv["発表"] == [] and alertLv["継続"] == [] and alertLv["解除"] != []:
+            tweetText = "#土砂災害警戒情報 は解除されました。\n地盤が緩んでいるところもあります。引き続き警戒を。"
+
+        id = uploadImage(wImagePath)
+
+        if args == ['main.py']:
+            tweet(tweetText,mediaIDs=[id])
+        elif args[1] == 'gitTest':
+            print(tweetText)
+
+        remove(wImagePath)
